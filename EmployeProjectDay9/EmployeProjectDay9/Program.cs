24cb42ed8e592f5fe0739e48d76a7396c0833dc6
Sangmeshmethre <ssmcmethre3080@gmail.com>
2022-09-29 12:45:49 +0530

    EmployeDay9

---
+++ EmployeProjectDay9/EmployeProjectDay9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EmployeProjectDay9
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //EmployCondition.EmployeDetail();
+            //EmployeWageUC2.CalculatingWage();
+            //Employetime.EmployeTime();
+            //Employecase.EmployeCase();
+            //Employemonth.EmployeMonth();
+            //EmployeworkingUC6.EmployeWorking();
+            //Employerefactor.computeEmpWage();
+
+            Employemultiplecompanies.computrEmpWage("DMart", 20, 2, 10);
+            Employemultiplecompanies.computrEmpWage("Reliance", 10, 4, 20);
+        }
+    }
+}
